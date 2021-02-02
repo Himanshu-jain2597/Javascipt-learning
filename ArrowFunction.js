622f633ed0012aca17161b146a647a6b8f9d4d12
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-02-02 14:03:44 +0530

    Update ArrowFunction.js

--- ArrowFunction.js
+++ ArrowFunction.js
@@ -1,4 +1,13 @@
 // return square of a number with normal function
+/**
+
+Does not have its own bindings to this or super, and should not be used as methods.
+Does not have arguments, or new.target keywords.
+Not suitable for call, apply and bind methods, which generally rely on establishing a scope.
+Can not be used as constructors.
+Can not use yield, within its body.
+
+*/ 
 const square =  function(number) {
 	return number* number;
 }
