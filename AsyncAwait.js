90e39dce1dfed552da031f6a0152ef684d98c207
Himanshu-jain2597 <42043162+Himanshu-jain2597@users.noreply.github.com>
2021-02-13 12:16:02 +0530

    Create AsyncAwait.js

---
+++ AsyncAwait.js
@@ -0,0 +1,37 @@
+
+const add = (a,b) => {
+
+    return new Promise((resolve,reject)=> {
+
+        setTimeout(()=> {
+            resolve(a+b);
+        },3000)
+    })
+
+}
+
+// asyync function return a promise and promise is fulfilled with return value of himanshu
+const doWork = async () => {
+    //throw new Error('something went wrong')  // this line will run a catch block
+    return 'himanshu'; // this will return then block.
+}
+
+doWork().then((result)=> {
+    console.log(result)
+}).catch((e)=> {
+    console.log(e);
+})
+
+const addValue  = async () => {
+    const sum =  await add(1,3);
+	console.log(`this is sum ${sum}`)
+	const sum2 =  await add(sum,1);
+	console.log(`add function ${sum2}`)
+    return sum2;
+}
+
+addValue().then((result)=> {
+console.log(`this is sum2 ${result}`);
+}).catch(()=> {
+    console.log('something went wrong');
+})
